303a536880fd9f54a4d5a1ed5489a68e75bfe3e4
Sergiu1-cyber <manzasergiu@gmail.com>
2022-02-20 18:49:32 +0200

    init

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+.about
+  h1 This is an about page
+  h3 Compilat cu pug
+  p pug funcționează bine
+</template>
+
+<script>
+  
+</script>
+
+<style lang="sass">
+h3
+  color: red
+</style>
